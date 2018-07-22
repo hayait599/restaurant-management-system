e1645fab8907eef9cdf6c1824152dc0271d08c2c
Bashar Qaddumi <38423515+BasharQaddumi@users.noreply.github.com>
2018-07-22 15:18:49 +0300

    Router added to Navbar

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -1,11 +1,11 @@
 import React , { Component } from 'react';
-
+import { NavLink } from 'react-router-dom';
 class Navbar extends Component {
 
     render () {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Restaurant</a>
+            <a className="navbar-brand" >Restaurant</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -13,22 +13,22 @@ class Navbar extends Component {
             <div className="collapse navbar-collapse" id="navbarColor03">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                  <NavLink to="/home"  className="nav-link" >Home <span className="sr-only">(current)</span></NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Restaurant</a>
+                  <NavLink to="/restaurant"  className="nav-link" >Restaurant</NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Menu</a>
+                  <NavLink to="/menu"  className="nav-link" >Menu</NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Order Online</a>
+                  <NavLink to="/order-online" className="nav-link" >Order Online</NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Team</a>
+                  <NavLink to="/team" className="nav-link" >Team</NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Contact</a>
+                  <NavLink to="/contact" className="nav-link" >Contact</NavLink>
                 </li>
               </ul>
               <form className="form-inline my-2 my-lg-0">
--- src/app/containers/HomePage.js
+++ src/app/containers/HomePage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
-
+import { Route, Switch } from 'react-router-dom';
 import Navbar from '../components/navbar/navbar';
 
 import '../../App.css';
@@ -9,14 +8,15 @@ class HomePage extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar></Navbar>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Navbar></Navbar>
+          <Switch>
+            <Route path="/home" />
+            <Route path="/restaurent" />
+            <Route path="/menu" />
+            <Route path="/order-online" />
+            <Route path="/team" />
+            <Route path="/contact" />
+          </Switch>
       </div>
     );
   }
