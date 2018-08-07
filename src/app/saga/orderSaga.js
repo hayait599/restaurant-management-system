140ba0b1a00a5b08be298503e6282b083410cdff
Bashar Qaddumi <38423515+BasharQaddumi@users.noreply.github.com>
2018-08-07 08:50:52 +0300

    Done

--- src/app/saga/orderSaga.js
+++ src/app/saga/orderSaga.js
@@ -1,7 +1,7 @@
 import 'regenerator-runtime/runtime';
-import { takeEvery, select } from 'redux-saga/effects';
+import { takeEvery, select, put } from 'redux-saga/effects';
 import axios from '../services/axios';
-import { order } from './../actions/index';
+import { order, selectIte, storeSelectedItem } from './../actions/index';
 
 function* orderFunction() {
   try {
@@ -14,9 +14,11 @@ function* orderFunction() {
        items: oldReddit.selectedItem,
        price: oldReddit.totalPrice
    }
+    const items  = yield select(storeSelectedItem);
    if(order.price !== 0)
    {
     yield axios.post('/Orders.json',order);
+    yield put(selectIte());
    }
    else{
        console.log(oldReddit);
